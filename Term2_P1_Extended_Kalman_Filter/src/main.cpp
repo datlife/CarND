1c87343715ccbf52907f7044e183c7fba83c623b
DAT <tdat.nguyen93@gmail.com>
2017-04-13 18:28:16 -0700

    Working EKF - Initial Update

--- Term2_P1_Extended_Kalman_Filter/src/main.cpp
+++ Term2_P1_Extended_Kalman_Filter/src/main.cpp
@@ -81,59 +81,74 @@ MatrixXd CalculateJacobian(const VectorXd& x_state){
     //recover state parameters
     float px = x_state(0);
     float py = x_state(1);
-    float vx = x_state(2);
-    float vy = x_state(3);
-
     //TODO: YOUR CODE HERE
     //check division by zero
     float distance = sqrt(px*px + py*py);
-
-    //compute the Jacobian matrix
-    if (distance != 0){
-        // Watch : Linearization non-linear models to understand Taylor series first
-        // Range Rho
-        float d_rho_posx = px / distance;
-        float d_rho_posy = py / distance;
-
-        // Angle thea
-        float d_theta_posx = - py / (distance*distance);
-        float d_theta_posy =   px / (distance*distance);
-        // Range Rate:
-        float d_rho_rate_posx = py*(vx*py - vy*px)/(distance*distance*distance);
-        float d_rho_rate_posy = px*(vy*px - vx*py)/(distance*distance*distance);
-        float d_rho_rate_velx = d_rho_posx;
-        float d_rho_rate_vely = d_rho_posy;
-
-        // Compute Jacobian matrix
-        Hj << d_rho_posx,      d_rho_posy,      0,     0,
-              d_theta_posx,    d_theta_posy,    0,     0,
-              d_rho_rate_posx, d_rho_rate_posy, d_rho_rate_velx, d_rho_rate_vely;
+    if (distance < 0.00001){
+        cout << "CalculateJacobian() error - division is zero  at...." <<endl;
+        return Hj;
     }
+    //compute the Jacobian matrix
+    float vx = x_state(2);
+    float vy = x_state(3);
+
+    // Watch : Linearization non-linear models to understand Taylor series first
+    // Range Rho
+    float d_rho_posx = px / distance;
+    float d_rho_posy = py / distance;
+
+    // Angle thea
+    float d_theta_posx = - py / (distance*distance);
+    float d_theta_posy =   px / (distance*distance);
+
+    // Range Rate:
+    float d_rho_rate_posx = py*(vx*py - vy*px)/(distance*distance*distance);
+    float d_rho_rate_posy = px*(vy*px - vx*py)/(distance*distance*distance);
+    float d_rho_rate_velx = d_rho_posx;
+    float d_rho_rate_vely = d_rho_posy;
+
+    // Compute Jacobian matrix
+    Hj << d_rho_posx,      d_rho_posy,      0,     0,
+          d_theta_posx,    d_theta_posy,    0,     0,
+          d_rho_rate_posx, d_rho_rate_posy, d_rho_rate_velx, d_rho_rate_vely;
+
     return Hj;
 }
 VectorXd CalculateRMSE(const vector<VectorXd> &estimations,
                        const vector<VectorXd> &ground_truth){
     VectorXd rmse(4);
+    // measure error of postion and velocity
     rmse << 0,0,0,0;
 
     // TODO: YOUR CODE HERE
 
     // check the validity of the following inputs:
     //  * the estimation vector size should not be zero
+    if (estimations.size() == 0 ){
+        cout << "No estimation is found.\n";
+        return rmse;
+    }
     //  * the estimation vector size should equal ground truth vector size
-    // ... your code here
+    if (estimations.size() != ground_truth.size()){
+       cout  << "Size mismatched between estimation and ground truth\n";
+        return rmse;
+    }
 
     //accumulate squared residuals
     for(int i=0; i < estimations.size(); ++i){
-        // ... your code here
+        // Calculate x - x_true
+        VectorXd residual = estimations[i] - ground_truth[i];
 
+        //coefficient-wise multiplication
+        // (x - x_true)^2
+        residual = residual.array()*residual.array();
+        rmse += residual;
     }
-
     //calculate the mean
-    // ... your code here
+    rmse = rmse / estimations.size();
 
     //calculate the squared root
-    // ... your code here
+    rmse = rmse.array().sqrt();
 
     //return the result
     return rmse;
